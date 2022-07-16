c9420695328b16de649e60dd9f167b423bd17867
Jacob Pratt <jacob@jhpratt.dev>
2022-07-16 00:25:41 -0400

    Add query to obtain `mut` restriction

--- compiler/rustc_restrictions/src/lib.rs
+++ compiler/rustc_restrictions/src/lib.rs
@@ -1,9 +1,11 @@
 #![doc(html_root_url = "https://doc.rust-lang.org/nightly/nightly-rustc/")]
 
 mod impl_restriction;
+mod mut_restriction;
 
 use rustc_middle::ty::query::Providers;
 
 pub fn provide(providers: &mut Providers) {
     impl_restriction::provide(providers);
+    mut_restriction::provide(providers);
 }
